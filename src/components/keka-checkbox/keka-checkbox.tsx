5adb68819f232d1ae131a9514d077de9c3fedc75
gunasekharn <sekhar.n@keka.com>
2023-07-10 11:44:54 +0530

    updated indeterminate state

--- src/components/keka-checkbox/keka-checkbox.tsx
+++ src/components/keka-checkbox/keka-checkbox.tsx
@@ -1,4 +1,4 @@
-import { Component, Event, h, Prop, EventEmitter, Listen } from '@stencil/core';
+import { Component, Event, h, Prop, EventEmitter, Listen, State } from '@stencil/core';
 
 @Component({
   tag: 'keka-checkbox',
@@ -6,25 +6,39 @@ import { Component, Event, h, Prop, EventEmitter, Listen } from '@stencil/core';
   shadow: true,
 })
 export class KekaCheckbox {
+
   @Prop({ mutable: true, reflect: true }) checked = false;
   @Prop({ mutable: true, reflect: true }) disabled = false;
-  @Prop() indeterminate: boolean = false;
-  @Prop() required: boolean = false;
+  @Prop({ mutable: true, reflect: true }) indeterminate: boolean = false;
+  @Prop() required:boolean =false;
 
   @Prop() mainText: string;
   @Prop() subText: string;
   @Prop() id: string;
 
-  @Prop() name: string = '';
-  @Prop() value: string = '';
+  @Prop() name: string="";
+  @Prop() value: string="";
+ 
 
   @Event() change: EventEmitter<boolean>;
   @Event() click: EventEmitter<void>;
   @Event() focus: EventEmitter<void>;
   @Event() blur: EventEmitter<void>;
 
-  private checkbox!: HTMLInputElement;
+  @State() state:string="checked";
 
+  private checkbox!: HTMLInputElement;
+  
+  componentWillLoad(){
+    if(this.indeterminate){
+      this.state="indeterminate"
+      this.checked=false
+    }
+    if(this.checked && this.indeterminate){
+      this.checked=false
+    }
+  }
+  
   @Listen('keydown')
   handleKeydown(ev: KeyboardEvent) {
     if (ev.code === 'Space') {
@@ -32,65 +46,72 @@ export class KekaCheckbox {
     }
   }
 
-  handleClick = () => {
-    if (!this.disabled) {
-      this.checked = !this.checked;
-      this.click.emit();
+  handleClick=()=> {
+    if(this.state=="indeterminate"){
+      if(!this.disabled){
+        this.indeterminate=!this.indeterminate
+        this.click.emit()
+      }
     }
-  };
 
-  handelChange = () => {
+    if(this.state=="checked"){
+      if(!this.disabled){
+        this.checked=!this.checked
+        this.click.emit()
+      }
+    }
+  }
+
+  handelChange=()=> {
     this.change.emit(this.checked);
-  };
+  }
 
-  handelFocus = () => {
-    this.focus.emit();
-  };
+  handelFocus=()=>{
+     this.focus.emit()
+  }
 
-  handelBlur = () => {
-    this.blur.emit();
-  };
+  handelBlur=()=>{
+      this.blur.emit()
+  }
 
   render() {
     const checkboxId = `checkbox-${this.id}`;
     return (
-      <div
-        class={{
-          container: true,
-          checked: this.checked,
-          disabled: this.disabled,
-        }}
-        role="checkbox"
-        aria-checked={this.checked ? true : false}
-        aria-disabled={this.disabled ? 'true' : 'false'}
-        tabindex="0"
-        onClick={this.handleClick}
-        onFocus={this.handelFocus}
-        onBlur={this.handelBlur}
-        aria-labelledby={`${checkboxId}-main-text ${checkboxId}-sub-text`}
+      <div 
+      class={{
+        container: true,
+        
+      }}
+      role="checkbox"
+      aria-checked={this.checked ? true :false}
+      aria-disabled={this.disabled ? true : false}
+      aria-indeterminate={this.indeterminate ? true : false}
+      tabindex="0"
+      onClick={this.handleClick}
+      onFocus={this.handelFocus}
+      onBlur={this.handelBlur}
+      aria-labelledby={`${checkboxId}-main-text ${checkboxId}-sub-text`}
       >
-      <input
-        type="checkbox"
-        class="checkbox"
-        ref={el => (this.checkbox = el)}
-        onChange={this.handelChange}
-        disabled={this.disabled}
-        checked={this.checked}
-        indeterminate={this.indeterminate}
-        required={this.required}
-        name={this.name}
-        value={this.value}
-        id={checkboxId}
-        tabindex="-1"
-      />
-      <div class="content">
-        <h4 class="main-text" id={`${checkboxId}-main-text`}>
-          {this.mainText}
-        </h4>
-        <p class="sub-text" id={`${checkboxId}-sub-text`}>
-          {this.subText}
-        </p>
-      </div>
+       
+          <input
+            type="checkbox"
+            class={{'checked':this.checked,'disabled':this.disabled,'indeterminate':this.indeterminate}}
+            ref={(el) => (this.checkbox = el)}
+            onChange={this.handelChange}
+            disabled={this.disabled}
+            checked={false} 
+            indeterminate={this.indeterminate}
+            required={this.required}
+            name={this.name}
+            value={this.value}
+            id={checkboxId}
+            tabindex="-1"
+            />
+      
+        <div class="content">
+          <h4 class="main-text" id={`${checkboxId}-main-text`} >{this.mainText}</h4>
+          <p class="sub-text" id={`${checkboxId}-sub-text`}>{this.subText}</p>
+        </div>
       </div>
     );
   }
